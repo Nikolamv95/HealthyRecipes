bdea8368bc60d2e6bbf90cbb06b93c00f731b4f5
Nikola Margaritov <nikola.margaritoov@gmail.com>
2021-04-17 15:00:07 +0300

    Scrape and add recipes

---
+++ Data/HealthyRecipes.Data/Migrations/20210414140711_OrinalUrl.cs
@@ -0,0 +1,23 @@
+﻿namespace HealthyRecipes.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class OrinalUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalUrl",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalUrl",
+                table: "Recipes");
+        }
+    }
+}
---
+++ Web/HealthyRecipes.Web/Controllers/GatherRecipesController.cs
@@ -0,0 +1,28 @@
+﻿namespace HealthyRecipes.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using HealthyRecipes.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    // TODO: Move in administration area
+    public class GatherRecipesController : BaseController
+    {
+        private readonly IGotvachBgScraperService gotvachBgScraperService;
+
+        public GatherRecipesController(IGotvachBgScraperService gotvachBgScraperService)
+        {
+            this.gotvachBgScraperService = gotvachBgScraperService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            await this.gotvachBgScraperService.ImportRecipesAsync();
+            return this.RedirectToAction("Index");
+        }
+    }
+}
