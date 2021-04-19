e9f8e57064fdcf9e4fd4b3f1a04aef8ae7a1f837
Nikola Margaritov <nikola.margaritoov@gmail.com>
2021-04-19 22:46:43 +0300

    Added Integration test

---
+++ Tests/HealthyRecipes.Services.Data.Tests/VoteServiceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace HealthyRecipes.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices.ComTypes;
+    using System.Threading.Tasks;
+
+    using HealthyRecipes.Data.Common.Repositories;
+    using HealthyRecipes.Data.Models;
+    using Moq;
+    using Xunit;
+
+    public class VoteServiceTests
+    {
+        [Fact]
+        public async Task WhenUserVotes2TimesOnly1VoteShouldBeCounted()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+
+            var service = new VotesService(mockRepo.Object);
+
+            await service.SetVoteAsync(1, "1", 1);
+            await service.SetVoteAsync(1, "1", 2);
+            await service.SetVoteAsync(1, "1", 3);
+            await service.SetVoteAsync(1, "1", 4);
+            await service.SetVoteAsync(1, "1", 5);
+
+            Assert.Single(list);
+            Assert.Equal(5, list.First().Value);
+        }
+
+        [Fact]
+        public async Task When2UserVotesForSameRecipeTheAverageVoteShouldBeCorrect()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+
+            var service = new VotesService(mockRepo.Object);
+
+            await service.SetVoteAsync(2, "Nikola", 5);
+            await service.SetVoteAsync(2, "Ivan", 1);
+            await service.SetVoteAsync(2, "Nikola", 2);
+
+            mockRepo.Verify(x => x.AddAsync(It.IsAny<Vote>()), Times.Exactly(2));
+
+            Assert.Equal(2, list.Count());
+            Assert.Equal(1.5, service.GetAverageVotes(2));
+        }
+    }
+}
